9415d0f5e9bbaa80010bd8b0847a15fba56ddaa2
federicormn <federico.roman@nybblegroup.com>
2022-11-09 09:26:15 -0300

    ADD --> TC: Edit Task (WORKING - TESTED - TO REPORT)

--- src/test/java/cleanTest/todoist/TasksTests.java
+++ src/test/java/cleanTest/todoist/TasksTests.java
@@ -52,4 +52,66 @@ public class TasksTests extends TestBaseTodoist
 
         Assertions.assertTrue(centerMenu.getLastTaskByName(taskName).isControlDisplayed());
     }
+
+    @Test
+    @DisplayName("Verify that a standard user can edit a task created inside an existing project.")
+    @Description("this test case is to verify that a standard user can edit a created task inside an existing project successfully.")
+    @Owner("Federico Roman")
+    @Epic("Tasks management")
+    @Feature("Task Edition")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Edit a Task Story")
+    @Tag("SmokeTest")
+    public void editTaskTest()
+    {
+        String taskDescription = "description";
+        String taskName = "task name";
+
+        //LOGIN AND SELECT LAST CREATED PROJECT (EXISTING)
+        mainPageTodoist.logInButton.click();
+        loginPage.login(GetProperties.getInstance().getUser(), GetProperties.getInstance().getPwd());
+        leftSideMenu.lastProject.click();
+
+        //CREATE TASK
+        centerMenu.addTaskButton.waitClickable();
+        centerMenu.addTaskButton.click();
+
+        centerMenu.projectCreationMenu.waitPresenceOfElement();
+        centerMenu.projectCreationMenu.waitClickable();
+        centerMenu.projectCreationMenu.waitVisibilityOfElement();
+
+        centerMenu.taskNameTextBox.writeText(taskName);
+
+        centerMenu.taskDescriptionTextBox.writeText(taskDescription);
+
+        centerMenu.dueDateButton.click();
+        centerMenu.tomorrowDueDateButton.click();
+
+        centerMenu.setPriorityButton.click();
+
+        centerMenu.pickPriority("1").click();
+
+        centerMenu.addTaskConfirmationButton.waitClickable();
+        centerMenu.addTaskConfirmationButton.click();
+
+        //EDIT TASK
+        centerMenu.getLastTaskByName(taskName).click();
+        String firstDueDate = editItemModal.editDueDateButton.getAttribute("textContent");
+        String firstPriority = editItemModal.selectNewPriority.getAttribute("textContent");
+        editItemModal.editDueDateButton.click();
+        editItemModal.nextWeekDueDateButton.click();
+
+        editItemModal.selectNewPriority.click();
+        editItemModal.choosePriority("2").click();
+        editItemModal.closeModal.click();
+
+        //Assertions for edition
+        centerMenu.getLastTaskByName(taskName).click();
+        String editedDueDate = editItemModal.editDueDateButton.getAttribute("textContent");
+        String editedPriority = editItemModal.selectNewPriority.getAttribute("textContent");
+        System.out.println("prioridades = "+ firstPriority + editedPriority);
+
+        Assertions.assertNotEquals(firstDueDate, editedDueDate);
+        Assertions.assertNotEquals(firstPriority,editedPriority);
+    }
 }
--- src/test/java/pages/todoist/EditItemModal.java
+++ src/test/java/pages/todoist/EditItemModal.java
@@ -6,8 +6,16 @@ import org.openqa.selenium.By;
 public class EditItemModal
 {
     public Button editDueDateButton = new Button(By.xpath("//button[@class=\"PkhZiTa _9KUWmzz a8af2163 _98cd5c3f _45ffe137 f9408a0e _8c75067a\"]"));
-    public Button nextWeekDueDateButton = new Button(By.xpath("//div[@class=\"scheduler-suggestions-item-label\" and text()=\"Next week\"]"));
-    public Button selectNewPriority = new Button(By.xpath("//div[@aria-label=\"Priority\"]//span[@class=\"bbdb467b f9408a0e d745aa1e _473810b4 _8c75067a\"]"));
+    //////////////////////
+    public Button todayDueDateButton = new Button(By.xpath("//button[@data-track=\"scheduler|date_shortcut_today\"]//div[@class=\"scheduler-suggestions-item-label\"]"));
+    public Button tomorrowDueDateButton = new Button(By.xpath("//button[@data-track=\"scheduler|date_shortcut_tomorrow\"]//div[@class=\"scheduler-suggestions-item-label\"]"));
+    public Button laterThisWeekDueDateButton = new Button(By.xpath("//button[@data-track=\"scheduler|date_shortcut_laterthisweekend\"]//div[@class=\"scheduler-suggestions-item-label\"]"));
+    public Button thisWeekendDueDateButton = new Button(By.xpath("//button[@data-track=\"scheduler|date_shortcut_thisweekend\"]//div[@class=\"scheduler-suggestions-item-label\"]"));
+    public Button nextWeekDueDateButton = new Button(By.xpath("//button[@data-track=\"scheduler|date_shortcut_nextweek\"]//div[@class=\"scheduler-suggestions-item-label\"]"));
+    public Button noDateDueDateButton = new Button(By.xpath("//button[@data-track=\"scheduler|date_shortcut_nodate\"]//div[@class=\"scheduler-suggestions-item-label\"]"));
+
+    ///////////////
+    public Button selectNewPriority = new Button(By.xpath("//div[contains(@aria-label,'Pri')]//button"));
 
     public Button pickPriority1 = new Button(By.xpath("//span[@class=\"priority_picker_item_name\" and text()=\"Priority 1\"]"));
     public Button pickPriority2 = new Button(By.xpath("//span[text()=\"Priority 2\"]"));
@@ -17,14 +25,13 @@ public class EditItemModal
     public Button moreActionsButton = new Button(By.xpath("//button[@aria-label=\"More actions\"]"));
     public Button deleteTask = new Button(By.xpath("//*[contains(text(),\"Delete task\")]"));
     public Button deleteConfirmationButton = new Button(By.xpath("//span[contains(text(),\"Delete\")]"));
-    public Button closeModal = new Button(By.xpath("//button[@aria-label=\"Close modal\"]"));
+    public Button closeModal = new Button(By.xpath("(//button[@class=\"a8af2163 _98cd5c3f _352995bd ef4c88db f9408a0e\"])[last()]"));
 
 
     public Button choosePriority(String priorityNumber)
     {
-        Button chosenPriority = new Button(By.xpath("//span[text()='Priority" +priorityNumber+"']"));
 
-        return chosenPriority;
+        return new Button(By.xpath("//span[contains(text(),'"+priorityNumber+"')]"));
     }
 
 }
